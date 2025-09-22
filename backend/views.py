369622434af47b0f44926b4b531fa8f9ec2d5fb4
okoriek <okoriek55@gmail.com>
2025-09-22 02:50:59 +0100

    latest update code now

--- backend/views.py
+++ backend/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, 'backend/index.html')
+
+
+def contact(request):
+
+    return render(request, 'backend/contact.html')
